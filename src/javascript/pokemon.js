349d46b674dc2dd164679077e215ef9f3d64f375
Yeampier2022 <yeampierjesusa@gmail.com>
2023-10-19 13:35:46 -0400

    fix delete script pokemon

--- src/javascript/pokemon.js
+++ src/javascript/pokemon.js
@@ -69,3 +69,17 @@ botonesHeader.forEach(boton => boton.addEventListener("click", (event) => {
     }
 }))
 
+const user = JSON.parse(localStorage.getItem("currentUser")) || false;
+
+if (!user) {
+  window.location.href = "../index.html";
+}
+
+const logout = document.querySelector("#logout");
+
+logout.addEventListener("click", () => {
+  alert("You have been logged out");
+  localStorage.removeItem("currentUser");
+  window.location.href = "../index.html";
+} );
+
